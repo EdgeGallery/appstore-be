343a5ada9390af0af9f997dec5673e45f22c5907
lxyhiuwan <hw.luoxiaoyun@huawei.com>
2020-12-03 12:01:50 +0800

    appstore增加healthcheck

--- src/main/java/org/edgegallery/appstore/interfaces/AccessTokenFilter.java
+++ src/main/java/org/edgegallery/appstore/interfaces/AccessTokenFilter.java
@@ -49,50 +49,52 @@ public class AccessTokenFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
         throws ServletException, IOException {
-        String accessTokenStr = request.getHeader("access_token");
-        if (StringUtils.isEmpty(accessTokenStr)) {
-            LOGGER.error("Access token is empty");
-            response.sendError(HttpStatus.UNAUTHORIZED.value(), "Access token is empty");
-            return;
+        if (request.getRequestURI() == null || !request.getRequestURI().equals("/health")) {
+            String accessTokenStr = request.getHeader("access_token");
+            if (StringUtils.isEmpty(accessTokenStr)) {
+                LOGGER.error("Access token is empty");
+                response.sendError(HttpStatus.UNAUTHORIZED.value(), "Access token is empty");
+                return;
+            }
+            OAuth2AccessToken accessToken = jwtTokenStore.readAccessToken(accessTokenStr);
+            if (accessToken == null) {
+                LOGGER.error("Invalid access token.");
+                response.sendError(HttpStatus.UNAUTHORIZED.value(), "Invalid access token.");
+                return;
+            }
+            if (accessToken.isExpired()) {
+                LOGGER.error("Access token expired");
+                response.sendError(HttpStatus.UNAUTHORIZED.value(), "Access token expired");
+                return;
+            }
+            Map<String, Object> additionalInfoMap = accessToken.getAdditionalInformation();
+            if (additionalInfoMap == null) {
+                LOGGER.error("Invalid access token, Additional is null.");
+                response.sendError(HttpStatus.UNAUTHORIZED.value(), "Invalid access token, Additional is null.");
+                return;
+            }
+            String userIdFromRequest = request.getParameter("userId");
+            String userIdFromToken = additionalInfoMap.get("userId").toString();
+            if (!StringUtils.isEmpty(userIdFromRequest) && !userIdFromRequest.equals(userIdFromToken)) {
+                LOGGER.error("Illegal userId");
+                response.sendError(HttpStatus.UNAUTHORIZED.value(), "Illegal userId");
+                return;
+            }
+            String userNameFromRequest = request.getParameter("userName");
+            String userNameFromToken = additionalInfoMap.get("userName").toString();
+            if (!StringUtils.isEmpty(userNameFromRequest) && !userNameFromRequest.equals(userNameFromToken)) {
+                LOGGER.error("Illegal userName");
+                response.sendError(HttpStatus.UNAUTHORIZED.value(), "Illegal userName");
+                return;
+            }
+            OAuth2Authentication auth = jwtTokenStore.readAuthentication(accessToken);
+            if (auth == null) {
+                LOGGER.error("Invalid access token");
+                response.sendError(HttpStatus.UNAUTHORIZED.value(), "Invalid access token");
+                return;
+            }
+            SecurityContextHolder.getContext().setAuthentication(auth);
         }
-        OAuth2AccessToken accessToken = jwtTokenStore.readAccessToken(accessTokenStr);
-        if (accessToken == null) {
-            LOGGER.error("Invalid access token.");
-            response.sendError(HttpStatus.UNAUTHORIZED.value(), "Invalid access token.");
-            return;
-        }
-        if (accessToken.isExpired()) {
-            LOGGER.error("Access token expired");
-            response.sendError(HttpStatus.UNAUTHORIZED.value(), "Access token expired");
-            return;
-        }
-        Map<String, Object> additionalInfoMap = accessToken.getAdditionalInformation();
-        if (additionalInfoMap == null) {
-            LOGGER.error("Invalid access token, Additional is null.");
-            response.sendError(HttpStatus.UNAUTHORIZED.value(), "Invalid access token, Additional is null.");
-            return;
-        }
-        String userIdFromRequest = request.getParameter("userId");
-        String userIdFromToken = additionalInfoMap.get("userId").toString();
-        if (!StringUtils.isEmpty(userIdFromRequest) && !userIdFromRequest.equals(userIdFromToken)) {
-            LOGGER.error("Illegal userId");
-            response.sendError(HttpStatus.UNAUTHORIZED.value(), "Illegal userId");
-            return;
-        }
-        String userNameFromRequest = request.getParameter("userName");
-        String userNameFromToken = additionalInfoMap.get("userName").toString();
-        if (!StringUtils.isEmpty(userNameFromRequest) && !userNameFromRequest.equals(userNameFromToken)) {
-            LOGGER.error("Illegal userName");
-            response.sendError(HttpStatus.UNAUTHORIZED.value(), "Illegal userName");
-            return;
-        }
-        OAuth2Authentication auth = jwtTokenStore.readAuthentication(accessToken);
-        if (auth == null) {
-            LOGGER.error("Invalid access token");
-            response.sendError(HttpStatus.UNAUTHORIZED.value(), "Invalid access token");
-            return;
-        }
-        SecurityContextHolder.getContext().setAuthentication(auth);
         filterChain.doFilter(request, response);
     }
 }
