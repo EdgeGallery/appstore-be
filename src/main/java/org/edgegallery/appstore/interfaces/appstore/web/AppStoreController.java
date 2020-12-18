b424859a88efb1782e1479a2ce3159527335e43c
cuijianwei <cjwtyllr@163.com>
2020-12-18 18:08:42 +0800

    改为body方式请求接口

--- src/main/java/org/edgegallery/appstore/interfaces/appstore/web/AppStoreController.java
+++ src/main/java/org/edgegallery/appstore/interfaces/appstore/web/AppStoreController.java
@@ -6,6 +6,7 @@ import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import java.util.List;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
@@ -22,12 +23,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 
 @Controller
 @RestSchema(schemaId = "appStore")
-@RequestMapping("/mec/appstore/v1")
+@RequestMapping("/mec/appstore/poke")
 @Api(tags = {"APP Store Controller"})
 @Validated
 public class AppStoreController {
@@ -58,26 +60,32 @@ public class AppStoreController {
             @ApiResponse(code = 415, message = "Unprocessable MicroServiceInfo Entity ", response = String.class),
             @ApiResponse(code = 500, message = "resource grant error", response = String.class)
     })
-    public ResponseEntity<AppStoreDto> addAppStore(
-            @ApiParam(value = "app store name") @RequestParam("appStoreName")
+    public ResponseEntity<AppStoreDto> addAppStore(@RequestBody AppStoreDto appStoreDto
+            /*
+            @ApiParam(value = "app store name") @RequestPart("appStoreName")
+            @NotNull(message = "appStoreName should not be null.")
             @Pattern(regexp = REG_STORE_NAME) String appStoreName,
-            @ApiParam(value = "app store version") @RequestParam("appStoreVersion")
-            @Length(max = MAX_VERSION_LEN, min = 1) String appStoreVersion,
-            @ApiParam(value = "app store company") @RequestParam("company")
-            @Length(max = MAX_COMPANY_LEN, min = 1) String company,
-            @ApiParam(value = "app store url") @RequestParam("url")
-            @Length(max = MAX_URL_LEN, min = 1) String url,
-            @ApiParam(value = "app store schema", required = false) @RequestParam("schema")
+            @ApiParam(value = "app store version") @RequestPart("appStoreVersion")
+            @NotNull(message = "appStoreVersion should not be null.")
+            @Length(max = MAX_VERSION_LEN) String appStoreVersion,
+            @ApiParam(value = "app store company") @RequestPart("company")
+            @NotNull(message = "company should not be null.")
+            @Length(max = MAX_COMPANY_LEN) String company,
+            @ApiParam(value = "app store url") @RequestPart("url")
+            @NotNull(message = "url should not be null.")
+            @Length(max = MAX_URL_LEN) String url,
+            @ApiParam(value = "app store schema", required = false) @RequestPart("schema")
             @Pattern(regexp = REG_SCHEMA) String schema,
-            @ApiParam(value = "app store push interface") @RequestParam("appPushIntf")
-            @Length(max = MAX_URL_LEN, min = 1) String appPushIntf,
-            @ApiParam(value = "appd translate id") @RequestParam("appdTransId")
+            @ApiParam(value = "app store push interface") @RequestPart("appPushIntf")
+            @Length(max = MAX_URL_LEN) String appPushIntf,
+            @ApiParam(value = "appd translate id") @RequestPart("appdTransId")
+            @NotNull(message = "appdTransId should not be null.")
             @Pattern(regexp = REG_UUID) String appdTransId,
             @ApiParam(value = "app store description", required = false)
-            @RequestParam(value = "description", required = false)
-            @Length(max = MAX_DESC_LEN) String description) {
-        AppStoreDto appStoreDto = new AppStoreDto(null, appStoreName, appStoreVersion, company, url, schema,
-                appPushIntf, appdTransId, description, null, null);
+            @RequestPart(value = "description", required = false)
+            @Length(max = MAX_DESC_LEN) String description*/) {
+        /*AppStoreDto appStoreDto = new AppStoreDto(null, appStoreName, appStoreVersion, company, url, schema,
+                appPushIntf, appdTransId, description, null, null);*/
         return appStoreServiceFacade.addAppStore(appStoreDto);
     }
 
@@ -92,8 +100,8 @@ public class AppStoreController {
             @ApiResponse(code = 500, message = "resource grant error", response = String.class)
     })
     public ResponseEntity<String> deleteAppStore(
-            @ApiParam(value = "app store id") @PathVariable("appStoreId")
-            @Pattern(regexp = REG_UUID) String appStoreId) {
+            @ApiParam(value = "app store id") @PathVariable("appStoreId") @Pattern(regexp = REG_UUID)
+            @NotNull(message = "appStoreId should not be null.") String appStoreId) {
         return appStoreServiceFacade.deleteAppStore(appStoreId);
     }
 
@@ -107,27 +115,31 @@ public class AppStoreController {
             @ApiResponse(code = 415, message = "Unprocessable MicroServiceInfo Entity ", response = String.class),
             @ApiResponse(code = 500, message = "resource grant error", response = String.class)
     })
-    public ResponseEntity<AppStoreDto> editAppStore(
-            @PathVariable("appStoreId") @Pattern(regexp = REG_UUID) String appStoreId,
-            @ApiParam(value = "app store name") @RequestParam(value = "appStoreName", required = false)
+    public ResponseEntity<AppStoreDto> editAppStore(@RequestBody AppStoreDto appStoreDto,
+            @PathVariable("appStoreId") @Pattern(regexp = REG_UUID)
+            @NotNull(message = "appStoreId should not be null.") String appStoreId
+            /*
+            @ApiParam(value = "app store name") @RequestPart(value = "appStoreName", required = false)
             @Pattern(regexp = REG_STORE_NAME) String appStoreName,
-            @ApiParam(value = "app store version") @RequestParam(value = "appStoreVersion", required = false)
+            @ApiParam(value = "app store version") @RequestPart(value = "appStoreVersion", required = false)
             @Length(max = MAX_VERSION_LEN, min = 1) String appStoreVersion,
-            @ApiParam(value = "app store company") @RequestParam(value = "company", required = false)
+            @ApiParam(value = "app store company") @RequestPart(value = "company", required = false)
             @Length(max = MAX_COMPANY_LEN, min = 1) String company,
-            @ApiParam(value = "app store url") @RequestParam(value = "url", required = false)
+            @ApiParam(value = "app store url") @RequestPart(value = "url", required = false)
             @Length(max = MAX_URL_LEN, min = 1) String url,
-            @ApiParam(value = "app store schema") @RequestParam(value = "schema", required = false)
+            @ApiParam(value = "app store schema") @RequestPart(value = "schema", required = false)
             @Pattern(regexp = REG_SCHEMA) String schema,
-            @ApiParam(value = "app push interface") @RequestParam(value = "appPushIntf", required = false)
+            @ApiParam(value = "app push interface") @RequestPart(value = "appPushIntf", required = false)
             @Length(max = MAX_URL_LEN, min = 1) String appPushIntf,
-            @ApiParam(value = "appd translate id") @RequestParam(value = "appdTransId", required = false)
+            @ApiParam(value = "appd translate id") @RequestPart(value = "appdTransId", required = false)
             @Pattern(regexp = REG_UUID) String appdTransId,
-            @ApiParam(value = "app store description") @RequestParam(value = "description", required = false)
-            @Length(max = MAX_DESC_LEN) String description) {
+            @ApiParam(value = "app store description") @RequestPart(value = "description", required = false)
+            @Length(max = MAX_DESC_LEN) String description*/) {
+        /*
         AppStoreDto appStoreDto = new AppStoreDto(appStoreId, appStoreName, appStoreVersion,
                 company, url, schema, appPushIntf, appdTransId, description,
-                null, null);
+                null, null);*/
+        appStoreDto.setAppStoreId(appStoreId);
         return appStoreServiceFacade.editAppStore(appStoreDto);
     }
 
@@ -158,8 +170,8 @@ public class AppStoreController {
             @ApiResponse(code = 500, message = "resource grant error", response = String.class)
     })
     public ResponseEntity<AppStoreDto> queryAppStore(
-            @ApiParam(value = "app store id") @PathVariable("appStoreId")
-            @Pattern(regexp = REG_UUID) String appStoreId) {
+            @ApiParam(value = "app store id") @PathVariable("appStoreId") @Pattern(regexp = REG_UUID)
+            @NotNull(message = "appStoreId should not be null.") String appStoreId) {
         return appStoreServiceFacade.queryAppStore(appStoreId);
     }
 }
