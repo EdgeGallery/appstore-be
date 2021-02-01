bf7804db76987b56f193fdcdde0e69b8e7814642
zhaolongfei_123 <951084082@qq.com>
2021-02-01 09:53:04 +0800

    修改问题，#I2E1U4 【Appstore】应用共享界面优化

--- src/main/java/org/edgegallery/appstore/interfaces/appstore/facade/AppStoreServiceFacade.java
+++ src/main/java/org/edgegallery/appstore/interfaces/appstore/facade/AppStoreServiceFacade.java
@@ -27,7 +27,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
 import javax.servlet.http.HttpServletRequest;
 
 @Service("AppStoreServiceFacade")
