d7726cf110ce41506244360abda2d9f25ff35dc1
luoxiaoyun <hw.luoxiaoyun@huawei.com>
2021-03-01 15:36:32 +0800

    去掉API接口对用户名的校验，解决admin用户不能上传应用问题

--- src/main/java/org/edgegallery/appstore/application/inner/AppService.java
+++ src/main/java/org/edgegallery/appstore/application/inner/AppService.java
@@ -18,7 +18,6 @@ package org.edgegallery.appstore.application.inner;
 
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.command.PullImageResultCallback;
-import com.github.dockerjava.api.exception.ConflictException;
 import com.github.dockerjava.api.exception.InternalServerErrorException;
 import com.github.dockerjava.api.exception.NotFoundException;
 import com.github.dockerjava.core.DefaultDockerClientConfig;
