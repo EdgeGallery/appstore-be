68840cda2a6adb514f5f7009d3d70738aee01054
luoxiaoyun <hw.luoxiaoyun@huawei.com>
2021-04-26 20:22:39 +0800

    https://gitee.com/edgegallery/appstore-be/issues/I3IL7I数据库增加显示类型字段

--- src/test/resources/appstore-test.sql
+++ src/test/resources/appstore-test.sql
@@ -6,50 +6,52 @@ DROP TABLE IF EXISTS PUSHABLE_PACKAGE_TABLE;
 DROP TABLE IF EXISTS message_table;
 
     create TABLE if not exists catalog_package_table (
-    	PACKAGEID                VARCHAR(200)       NOT NULL,
-    	PACKAGEADDRESS           VARCHAR(200)       NULL,
-    	ICONADDRESS              VARCHAR(200)       NULL,
-    	DEMOVIDEOADDRESS         VARCHAR(200)       NULL,
-    	SIZE                     VARCHAR(100)       NULL,
-    	FILESTRUCTURE            TEXT               NULL,
-    	CREATETIME               TIMESTAMP          NULL,
-    	SHORTDESC	             TEXT		        NULL,
-    	APPNAME                  VARCHAR(100)       NULL,
-    	VERSION                  VARCHAR(20)        NULL,
-    	APPLICATIONTYPE          VARCHAR(300)       NULL,
+        PACKAGEID                VARCHAR(200)       NOT NULL,
+        PACKAGEADDRESS           VARCHAR(200)       NULL,
+        ICONADDRESS              VARCHAR(200)       NULL,
+        DEMOVIDEOADDRESS         VARCHAR(200)       NULL,
+        SIZE                     VARCHAR(100)       NULL,
+        FILESTRUCTURE            TEXT               NULL,
+        CREATETIME               TIMESTAMP          NULL,
+        SHORTDESC                TEXT               NULL,
+        APPNAME                  VARCHAR(100)       NULL,
+        VERSION                  VARCHAR(20)        NULL,
+        APPLICATIONTYPE          VARCHAR(300)       NULL,
         DEPLOYMODE               VARCHAR(100)       NULL,
-    	MARKDOWNCONTENT          TEXT			    NULL,
-    	AFFINITY                 VARCHAR(100)       NULL,
-    	INDUSTRY                 VARCHAR(100)       NULL,
-    	CONTACT                  VARCHAR(100)       NULL,
-    	APPID                    VARCHAR(100)       NULL,
-    	USERID                   VARCHAR(100)       NULL,
-    	USERNAME                 VARCHAR(100)       NULL,
-    	TESTTASKID               VARCHAR(100)       NULL,
-    	STATUS                   VARCHAR(100)       NULL,
-    	PROVIDER                 VARCHAR(100)       NULL,
-    	CONSTRAINT catalog_package_table_pkey PRIMARY KEY (PACKAGEID)
+        MARKDOWNCONTENT          TEXT               NULL,
+        AFFINITY                 VARCHAR(100)       NULL,
+        INDUSTRY                 VARCHAR(100)       NULL,
+        CONTACT                  VARCHAR(100)       NULL,
+        APPID                    VARCHAR(100)       NULL,
+        USERID                   VARCHAR(100)       NULL,
+        USERNAME                 VARCHAR(100)       NULL,
+        TESTTASKID               VARCHAR(100)       NULL,
+        STATUS                   VARCHAR(100)       NULL,
+        PROVIDER                 VARCHAR(100)       NULL,
+        SHOWTYPE                 VARCHAR(100)       NOT NULL DEFAULT 'public',
+        CONSTRAINT catalog_package_table_pkey PRIMARY KEY (PACKAGEID)
     );
 
     create TABLE if not exists app_table (
-    	APPID                    VARCHAR(200)       NOT NULL,
-    	APPNAME                  VARCHAR(100)       NULL,
-    	APPLICATIONTYPE          VARCHAR(300)       NULL,
+        APPID                    VARCHAR(200)       NOT NULL,
+        APPNAME                  VARCHAR(100)       NULL,
+        APPLICATIONTYPE          VARCHAR(300)       NULL,
         DEPLOYMODE               VARCHAR(100)       NULL,
-    	SHORTDESC	             TEXT		        NULL,
-    	PROVIDER                 VARCHAR(300)       NULL,
-    	APPINTRODUCTION		     TEXT			    NULL,
-    	DOWNLOADCOUNT            INT                NULL,
-    	AFFINITY                 VARCHAR(100)       NULL,
-    	INDUSTRY                 VARCHAR(100)       NULL,
-    	CONTACT                  VARCHAR(100)       NULL,
-    	USERID                   VARCHAR(100)       NULL,
-    	USERNAME                 VARCHAR(100)       NULL,
-    	CREATETIME               TIMESTAMP          NULL,
-    	MODIFYTIME               TIMESTAMP          NULL,
+        SHORTDESC                TEXT               NULL,
+        PROVIDER                 VARCHAR(300)       NULL,
+        APPINTRODUCTION          TEXT           NULL,
+        DOWNLOADCOUNT            INT                NULL,
+        AFFINITY                 VARCHAR(100)       NULL,
+        INDUSTRY                 VARCHAR(100)       NULL,
+        CONTACT                  VARCHAR(100)       NULL,
+        USERID                   VARCHAR(100)       NULL,
+        USERNAME                 VARCHAR(100)       NULL,
+        CREATETIME               TIMESTAMP          NULL,
+        MODIFYTIME               TIMESTAMP          NULL,
         SCORE                    NUMERIC(2,1)       NULL,
         STATUS                   VARCHAR(50)       NULL,
-    	CONSTRAINT app_table_pkey PRIMARY KEY (APPID)
+        SHOWTYPE                 VARCHAR(100)       NOT NULL DEFAULT 'public',
+        CONSTRAINT app_table_pkey PRIMARY KEY (APPID)
     );
 
     create TABLE if not exists csar_package_score (
@@ -113,27 +115,27 @@ DROP TABLE IF EXISTS message_table;
     );
 
 INSERT INTO app_table(
-	appid, appname, applicationtype, shortdesc, provider, appintroduction, downloadcount, affinity, industry, contact, userid, username, createtime, modifytime, score, STATUS)
-	VALUES ('appid-test-0001', 'app-001', 'game', 'shortdesc', 'provider', 'appintroduction', 5, 'affinity', 'industry', 'contactcontact', 'test-userid-0001', 'test-username-0001', now(), now(), 3.2, 'Published');
+    appid, appname, applicationtype, shortdesc, provider, appintroduction, downloadcount, affinity, industry, contact, userid, username, createtime, modifytime, score, STATUS)
+    VALUES ('appid-test-0001', 'app-001', 'game', 'shortdesc', 'provider', 'appintroduction', 5, 'affinity', 'industry', 'contactcontact', 'test-userid-0001', 'test-username-0001', now(), now(), 3.2, 'Published');
 
 INSERT INTO catalog_package_table(
-	packageid, packageaddress, iconaddress, demovideoaddress, size, filestructure, createtime, shortdesc, appname,
-	version, applicationtype, markdowncontent, affinity, industry, contact, appid, userid, username, status, TESTTASKID, PROVIDER)
-	VALUES ('packageid-0003', '/package/test/face.csar', '/user/test/icon.png', '', 10004, 'file-trees', now(),
-	'shortdesc', 'appname', 'version', 'game', 'markdowncontent', 'affinity', 'industry', 'contact', 'appid-test-0001',
-	'test-userid-0001', 'test-username-0002', 'Published', 'apt-taskid-0002', 'PROVIDER');
+    packageid, packageaddress, iconaddress, demovideoaddress, size, filestructure, createtime, shortdesc, appname,
+    version, applicationtype, markdowncontent, affinity, industry, contact, appid, userid, username, status, TESTTASKID, PROVIDER)
+    VALUES ('packageid-0003', '/package/test/face.csar', '/user/test/icon.png', '', 10004, 'file-trees', now(),
+    'shortdesc', 'appname', 'version', 'game', 'markdowncontent', 'affinity', 'industry', 'contact', 'appid-test-0001',
+    'test-userid-0001', 'test-username-0002', 'Published', 'apt-taskid-0002', 'PROVIDER');
 
 INSERT INTO catalog_package_table(
-	packageid, packageaddress, iconaddress, demovideoaddress, size, filestructure, createtime, shortdesc, appname,
-	version, applicationtype, markdowncontent, affinity, industry, contact, appid, userid, username, status, TESTTASKID, PROVIDER)
-	VALUES ('packageid-0002', '/package/test/face.csar', '/user/test/icon.png', '', 10002, 'file-trees', now(),
-	'shortdesc', 'appname', 'version', 'game', 'markdowncontent', 'affinity', 'industry', 'contact', 'appid-test-0001',
-	'test-userid-0001', 'test-username-0001', 'Published', 'apt-taskid-0001', 'PROVIDER');
+    packageid, packageaddress, iconaddress, demovideoaddress, size, filestructure, createtime, shortdesc, appname,
+    version, applicationtype, markdowncontent, affinity, industry, contact, appid, userid, username, status, TESTTASKID, PROVIDER)
+    VALUES ('packageid-0002', '/package/test/face.csar', '/user/test/icon.png', '', 10002, 'file-trees', now(),
+    'shortdesc', 'appname', 'version', 'game', 'markdowncontent', 'affinity', 'industry', 'contact', 'appid-test-0001',
+    'test-userid-0001', 'test-username-0001', 'Published', 'apt-taskid-0001', 'PROVIDER');
 
 INSERT INTO app_store_table(
-	appstoreid, APPSTORENAME, APPSTOREVERSION, company, url, schema, apppushintf, APPDTRANSID, addedtime, modifiedtime, description)
-	VALUES ('a09bca74-04cb-4bae-9ee2-9c5072ec9d4b', 'liantong', 'v1.0', 'liantong', 'http://127.0.0.1:8099', 'http', '', '', now(), null, 'description-5555');
+    appstoreid, APPSTORENAME, APPSTOREVERSION, company, url, schema, apppushintf, APPDTRANSID, addedtime, modifiedtime, description)
+    VALUES ('a09bca74-04cb-4bae-9ee2-9c5072ec9d4b', 'liantong', 'v1.0', 'liantong', 'http://127.0.0.1:8099', 'http', '', '', now(), null, 'description-5555');
 
 INSERT INTO app_store_table(
-	appstoreid, APPSTORENAME, APPSTOREVERSION, company, url, schema, apppushintf, APPDTRANSID, addedtime, modifiedtime, description)
-	VALUES ('02ef9eeb-d50e-4835-8d05-e5fdb87b7596', '移动', 'v1.0', '移动', 'http://127.0.0.1:8099', 'http', '', '', now(), null, 'description-5555');
+    appstoreid, APPSTORENAME, APPSTOREVERSION, company, url, schema, apppushintf, APPDTRANSID, addedtime, modifiedtime, description)
+    VALUES ('02ef9eeb-d50e-4835-8d05-e5fdb87b7596', '移动', 'v1.0', '移动', 'http://127.0.0.1:8099', 'http', '', '', now(), null, 'description-5555');
