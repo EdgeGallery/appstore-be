d07e5b78776859efd6044543c005ca4383c98387
luoxiaoyun <hw.luoxiaoyun@huawei.com>
2021-05-25 11:16:30 +0800

    回退//修改应用包保存服务器的方式，保证文件不被修改。

--- src/main/java/org/edgegallery/appstore/domain/model/releases/PackageChecker.java
+++ src/main/java/org/edgegallery/appstore/domain/model/releases/PackageChecker.java
@@ -78,11 +78,8 @@ public class PackageChecker extends FileChecker {
             .append(File.separator).append(file.getOriginalFilename()).toString();
         try {
             createFile(tempFileAddress);
-            try (FileOutputStream fos = new FileOutputStream(tempFileAddress)) {
-                byte[] bytes = file.getBytes();
-                fos.write(bytes);
-            }
             result = new File(tempFileAddress);
+            file.transferTo(result);
             unzip(tempFileAddress);
         } catch (IOException e) {
             LOGGER.error("create temp file failed: {}", e.getMessage());
