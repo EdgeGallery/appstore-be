82b6cb9778636deb234c63f27408217d3c658796
zqy <zhangqiyu2@huawei.com>
2021-06-29 20:20:46 +0800

    补充ut

---
+++ src/test/java/org/edgegallery/appstore/interfaces/app/web/DeployAppByIdTest.java
@@ -0,0 +1,58 @@
+/* Copyright 2020 Huawei Technologies Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.edgegallery.appstore.interfaces.app.web;
+
+import com.spencerwi.either.Either;
+import org.edgegallery.appstore.domain.shared.ResponseObject;
+import org.edgegallery.appstore.interfaces.AppTest;
+import org.edgegallery.appstore.interfaces.app.facade.dto.RegisterRespDto;
+import org.edgegallery.appstore.interfaces.system.facade.ProjectService;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+public class DeployAppByIdTest extends AppTest {
+
+    @Autowired
+    private ProjectService projectService;
+
+    @Before
+    public void init() {
+        System.out.println("start to test");
+    }
+
+    @After
+    public void after() {
+        System.out.println("test over");
+    }
+
+    @Test
+    @WithMockUser(roles = "APPSTORE_TENANT")
+    public void should_failed_no_appInstanceId() {
+        ResponseEntity<ResponseObject> res  = projectService.deployAppById("appid-test-0001", "packageid-0002", "e111f3e7-90d8-4a39-9874-ea6ea6752eaa", "host-1", null, "access_token");
+        Assert.assertEquals("instantiate application failed.", res.getBody().getMessage());
+    }
+
+}
