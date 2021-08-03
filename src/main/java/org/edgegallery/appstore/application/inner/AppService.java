9abf2b98a122c0a88cd70cc982fb3d798b12ae0f
luoxiaoyun <hw.luoxiaoyun@huawei.com>
2021-08-03 19:33:57 +0800

    I41V3A【AppStore】删除应用包同时删除其测试报告、可推送应用列表中数据 --- 增加调试日志

--- src/main/java/org/edgegallery/appstore/application/inner/AppService.java
+++ src/main/java/org/edgegallery/appstore/application/inner/AppService.java
@@ -583,12 +583,17 @@ public class AppService {
             .orElseThrow(() -> new UnknownReleaseExecption(packageId, ResponseConst.RET_PACKAGE_NOT_FOUND));
         release.checkPermission(user);
 
+        LOGGER.info("unPublishPackage appId test 1111");
         app.unPublish(release);
         if (app.getReleases().isEmpty()) {
+            LOGGER.info("unPublishPackage appId test 2222");
             unPublish(app, token);
         } else {
+            LOGGER.info("unPublishPackage appId test 3333");
             packageRepository.removeRelease(release);
+            LOGGER.info("unPublishPackage appId test 4444");
             deletePullablePackage(release);
+            LOGGER.info("unPublishPackage appId test 5555");
             deleteTestReport(release, token);
             if (!app.hasPublishedRelease()) {
                 app.setStatus(EnumAppStatus.UnPublish);
