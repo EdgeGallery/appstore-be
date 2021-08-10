71ea335a0e7a133bb632b72fcb8ff186aea4684b
鹿鑫 <luxin7@huawei.com>
2021-08-10 15:09:05 +0800

    Add entity

---
+++ src/main/java/org/edgegallery/appstore/application/packageupload/UploadPackageEntity.java
@@ -0,0 +1,21 @@
+package org.edgegallery.appstore.application.packageupload;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class UploadPackageEntity {
+    private String fileName;
+
+    private byte[] postData;
+
+    private long totalSie;
+
+    private int shardCount;
+
+    private String csrfToken;
+
+    private String cookie;
+
+}
