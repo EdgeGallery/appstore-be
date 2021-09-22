dacbd6938fe385c92952eecfe7a53040c530c28b
鹿鑫 <luxin7@huawei.com>
2021-09-22 14:24:41 +0800

    Update progress status failed when upload fail.

--- src/main/java/org/edgegallery/appstore/application/packageupload/UploadHelper.java
+++ src/main/java/org/edgegallery/appstore/application/packageupload/UploadHelper.java
@@ -102,8 +102,7 @@ public class UploadHelper {
                 upPackage.setShardCount(count);
                 ret = Connection.postFiles(header, "https://" + hostUrl + url, upPackage, req, buffer);
                 if (ret.getInteger("retCode") == -1) {
-                    progress.setStatus("fail");
-                    progressFacade.updateProgress(progress);
+                    updateProgressStatus(progress, "failed");
                     LOGGER.error("upload failed: {}", ret.toString());
                     return ret;
                 }
@@ -123,6 +122,7 @@ public class UploadHelper {
             byte[] ednBuffer = new byte[(int) length];
             int readCount = input.read(ednBuffer);
             if (readCount == -1) {
+                updateProgressStatus(progress, "failed");
                 LOGGER.error("upload failed: {}", ret.toString());
                 return ret;
             }
@@ -137,8 +137,7 @@ public class UploadHelper {
 
             // update upload progress to 100%
             progress.setProgress("100");
-            progress.setStatus("success");
-            progressFacade.updateProgress(progress);
+            updateProgressStatus(progress, "success");
 
             return ret;
         } catch (IOException e) {
@@ -152,10 +151,14 @@ public class UploadHelper {
                 LOGGER.error("uploadBigSoftware close input IOException");
             }
         }
-        progress.setStatus("fail");
-        progressFacade.updateProgress(progress);
+        updateProgressStatus(progress, "failed");
         ret.put("retCode", -1);
         LOGGER.error("upload failed: {}", ret.toString());
         return ret;
+    }
+
+    private void updateProgressStatus(PackageUploadProgress progress, String status) {
+        progress.setStatus(status);
+        progressFacade.updateProgress(progress);
     }
 }
