588f74a0874aec35f876e984ddc7b1c33a17bade
13165725752@163.com <13165725752@163.com>
2021-09-29 10:02:00 +0800

    定时删除代码优化

--- src/main/java/org/edgegallery/appstore/infrastructure/util/ScheduleTask.java
+++ src/main/java/org/edgegallery/appstore/infrastructure/util/ScheduleTask.java
@@ -39,7 +39,7 @@ public class ScheduleTask {
     }
 
     // @Scheduled(cron = "0 0 0 * * ? ")
-    @Scheduled(cron = "30 40 16 * * ? ")
+    @Scheduled(cron = "30 20 11 * * ? ")
     public void processCleanTempPackage() {
         packageServiceFacade.scheduledDeletePackage();
     }
