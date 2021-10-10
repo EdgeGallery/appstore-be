73c20879d88283ea4d5a78c04eb3a6362a1aae48
zhangbeiyuan <zhangbeiyuan@huawei.com>
2021-10-10 15:25:51 +0800

    add UT for AtpUtilTest

---
+++ src/test/java/org/edgegallery/appstore/interfaces/atp/AtpTaskDto.java
@@ -0,0 +1,11 @@
+package org.edgegallery.appstore.interfaces.atp;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AtpTaskDto {
+    private String id;
+    private String status;
+}
