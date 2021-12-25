fc5216898bb5bd57e58ab2161063b23d383316f8
Miranda <qingshuting1@huawei.com>
2021-12-25 17:24:00 +0800

    delete a useless statement

--- src/main/java/org/edgegallery/appstore/application/inner/OrderService.java
+++ src/main/java/org/edgegallery/appstore/application/inner/OrderService.java
@@ -97,7 +97,6 @@ public class OrderService {
             LOGGER.error("Distributed or Instantiated failed, modify status to activate failed");
         }
         orderRepository.updateOrder(order);
-        return;
     }
 
     /**
