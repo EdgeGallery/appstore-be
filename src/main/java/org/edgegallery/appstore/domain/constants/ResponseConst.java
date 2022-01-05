62476ae5d6b76ceead8cd31e6ec7cd154f2c6db8
13165725752@163.com <13165725752@163.com>
2022-01-05 11:36:53 +0800

    我的应用操作权限优化

--- src/main/java/org/edgegallery/appstore/domain/constants/ResponseConst.java
+++ src/main/java/org/edgegallery/appstore/domain/constants/ResponseConst.java
@@ -525,6 +525,11 @@ public class ResponseConst {
     public static final int RET_NO_ACCESS_ACTIVATE_ORDER = 18004;
 
     /**
+     * Permission not allowed to delete application package.
+     */
+    public static final int RET_NO_ACCESS_MODIFY_PACKAGE = 18003;
+
+    /**
      * general base error code.
      */
     public static final int RET_GENERAL_BASE = 19000;
